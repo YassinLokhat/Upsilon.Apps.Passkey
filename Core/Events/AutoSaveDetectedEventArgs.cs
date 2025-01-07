9a812f0d8e3a13816bfae5317e076618cab50118
Yassin Lokhat <yassinlokhat@gmail.com>
2025-01-07 22:31:04 +0300

    Events comments

--- Core/Events/AutoSaveDetectedEventArgs.cs
+++ Core/Events/AutoSaveDetectedEventArgs.cs
@@ -2,10 +2,15 @@
 
 namespace Upsilon.Apps.PassKey.Core.Events
 {
+   /// <summary>
+   /// Represent the behavior of auto-save handling event argument.
+   /// </summary>
    public class AutoSaveDetectedEventArgs : EventArgs
    {
+      /// <summary>
+      /// The behavior selected.
+      /// By default it will merge then remove the auto-save file.
+      /// </summary>
       public AutoSaveMergeBehavior MergeBehavior { get; set; } = AutoSaveMergeBehavior.MergeThenRemoveAutoSaveFile;
-
-      public AutoSaveDetectedEventArgs() : base() { }
    }
 }
