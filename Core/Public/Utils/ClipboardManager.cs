9502a269e4307b031c72f5fcef34d5e2e581d955
Yassin Lokhat <yassinlokhat@gmail.com>
2025-09-22 13:00:30 +0300

    Adding ClipboardManager to remove all occurence of passwords

---
+++ Core/Public/Utils/ClipboardManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upsilon.Apps.PassKey.Core.Public.Interfaces;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Upsilon.Apps.PassKey.Core.Public.Utils
+{
+   public static class ClipboardManager
+   {
+      public static void RemoveAllOccurence(string[] removeList)
+      {
+         var clipboardHistory = Clipboard.GetHistoryItemsAsync().AsTask().GetAwaiter().GetResult().Items;
+
+         foreach (var item in clipboardHistory)
+         {
+            var content = item.Content;
+            if (content.Contains(StandardDataFormats.Text))
+            {
+               string text = content.GetTextAsync().AsTask().GetAwaiter().GetResult();
+
+               if (removeList.Any(x => x == text))
+               {
+                  Clipboard.DeleteItemFromHistory(item);
+               }
+            }
+         }
+      }
+   }
+}
