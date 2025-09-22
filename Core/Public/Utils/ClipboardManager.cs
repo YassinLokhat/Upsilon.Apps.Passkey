9e09ec7c75a58f441e02fe328a0f95800aef185c
Yassin Lokhat <yassinlokhat@gmail.com>
2025-09-22 14:19:13 +0300

    Code Cleanup

--- Core/Public/Utils/ClipboardManager.cs
+++ Core/Public/Utils/ClipboardManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Upsilon.Apps.PassKey.Core.Public.Interfaces;
-using Windows.ApplicationModel.DataTransfer;
+﻿using Windows.ApplicationModel.DataTransfer;
 
 namespace Upsilon.Apps.PassKey.Core.Public.Utils
 {
