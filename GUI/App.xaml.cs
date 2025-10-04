f050a3e0444f5173616cd1efb87c83ff622da3e1
Yassin Lokhat <yassinlokhat@gmail.com>
2025-10-04 20:49:16 +0300

    Adding Upsilon.Apps.Passkey.GUI MAUI project

---
+++ GUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Upsilon.Apps.Passkey.GUI
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         MainPage = new AppShell();
+      }
+   }
+}
---
+++ GUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Upsilon.Apps.Passkey.GUI
+{
+   public partial class AppShell : Shell
+   {
+      public AppShell()
+      {
+         InitializeComponent();
+      }
+   }
+}
