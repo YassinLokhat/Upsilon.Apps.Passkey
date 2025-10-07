8c6ad379be8c1d7f375a1e167f3ed581b788a0c2
Yassin Lokhat <yassinlokhat@gmail.com>
2025-10-07 10:48:37 +0300

    Adding Dark theme

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,13 +1,17 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Upsilon.Apps.Passkey.GUI.ViewModels;
 
 namespace Upsilon.Apps.Passkey.GUI
 {
@@ -16,9 +20,61 @@ namespace Upsilon.Apps.Passkey.GUI
    /// </summary>
    public partial class MainWindow : Window
    {
+      public static string AppTitle
+      {
+         get
+         {
+            var package = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+            var packageVersion = package.Version?.ToString(2);
+
+            return $"{package.Name} v{packageVersion}";
+         }
+      }
+
       public MainWindow()
       {
          InitializeComponent();
+
+         DataContext = new MainViewModel { WindowTitle = AppTitle };
+
+         this.Loaded += MainWindow_Loaded;
       }
+
+      private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+      {
+         SetDarkMode();
+      }
+
+      #region SetDarkMode
+      private void SetDarkMode()
+      {
+         SetDarkTitleBar();
+
+         var dictionaries = Application.Current.Resources.MergedDictionaries;
+         dictionaries.Clear();
+
+         var themeDict = new ResourceDictionary
+         {
+            Source = new Uri($"Themes/DarkTheme.xaml", UriKind.Relative)
+         };
+
+         dictionaries.Add(themeDict);
+      }
+
+      private void SetDarkTitleBar()
+      {
+         var hwnd = new WindowInteropHelper(this).Handle;
+
+         if (hwnd == IntPtr.Zero)
+            return;
+
+         int attribute = 20; // DWMWA_USE_IMMERSIVE_DARK_MODE
+         int useImmersiveDarkMode = 1;
+         DwmSetWindowAttribute(hwnd, attribute, ref useImmersiveDarkMode, sizeof(int));
+      }
+
+      [DllImport("dwmapi.dll", PreserveSig = true)]
+      private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+      #endregion
    }
 }
---
+++ GUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upsilon.Apps.Passkey.GUI.ViewModels
+{
+   public class MainViewModel : INotifyPropertyChanged
+   {
+      private string _windowTitle = MainWindow.AppTitle;
+
+      public string WindowTitle
+      {
+         get => _windowTitle;
+         set
+         {
+            _windowTitle = value;
+            OnPropertyChanged(nameof(WindowTitle));
+         }
+      }
+
+      public event PropertyChangedEventHandler? PropertyChanged;
+
+      protected virtual void OnPropertyChanged(string propertyName)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+   }
+}
