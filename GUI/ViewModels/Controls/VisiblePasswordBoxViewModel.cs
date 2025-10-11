995e675185ef41e97563f5fc6503dcc373c807a8
Yassin Lokhat <yassinlokhat@gmail.com>
2025-10-11 21:19:10 +0300

    PrivateTextBox - Part 2

---
+++ GUI/ViewModels/Controls/VisiblePasswordBoxViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Upsilon.Apps.Passkey.GUI.ViewModels.Controls
+{
+   public class VisiblePasswordBoxViewModel : INotifyPropertyChanged
+   {
+      private string _password = string.Empty;
+      public string Password
+      {
+         get => _password;
+         set
+         {
+            if (_password != value)
+            {
+               _password = value;
+               OnPropertyChanged(nameof(Password));
+            }
+         }
+      }
+
+      private Visibility _passwordVisibility = Visibility.Visible;
+      public Visibility PasswordVisibility
+      {
+         get => _passwordVisibility;
+         set
+         {
+            if (_passwordVisibility != value)
+            {
+               _passwordVisibility = value;
+
+               OnPropertyChanged(nameof(PasswordVisibility));
+            }
+         }
+      }
+
+      private Visibility _textVisibility = Visibility.Hidden;
+      public Visibility TextVisibility
+      {
+         get => _textVisibility;
+         set
+         {
+            if (_textVisibility != value)
+            {
+               _textVisibility = value;
+
+               OnPropertyChanged(nameof(TextVisibility));
+            }
+         }
+      }
+
+      private Visibility _buttonVisibility = Visibility.Visible;
+      public Visibility ButtonVisibility
+      {
+         get => _buttonVisibility;
+         set
+         {
+            if (_buttonVisibility != value)
+            {
+               _buttonVisibility = value;
+
+               OnPropertyChanged(nameof(ButtonVisibility));
+            }
+         }
+      }
+
+      public bool PasswordIsVisible => PasswordVisibility == System.Windows.Visibility.Hidden;
+
+      public event PropertyChangedEventHandler? PropertyChanged;
+
+      protected virtual void OnPropertyChanged(string propertyName)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      public void TooglePasswordVisibility()
+      {
+         if (!PasswordIsVisible)
+         {
+            PasswordVisibility = System.Windows.Visibility.Hidden;
+            TextVisibility = System.Windows.Visibility.Visible;
+         }
+         else
+         {
+            PasswordVisibility = System.Windows.Visibility.Visible;
+            TextVisibility = System.Windows.Visibility.Hidden;
+         }
+      }
+   }
+}
---
+++ GUI/Views/Controls/VisiblePasswordBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+using Upsilon.Apps.Passkey.GUI.ViewModels.Controls;
+
+namespace Upsilon.Apps.Passkey.GUI.Views.Controls
+{
+   /// <summary>
+   /// Interaction logic for PrivateTextBox.xaml
+   /// </summary>
+   public partial class VisiblePasswordBox : UserControl
+   {
+      private readonly PasswordBox _passwordBox;
+      private readonly VisiblePasswordBoxViewModel _privateTextBoxViewModel;
+
+      public VisiblePasswordBox()
+      {
+         InitializeComponent();
+
+         DataContext = _privateTextBoxViewModel = new VisiblePasswordBoxViewModel();
+
+         _passwordBox = (PasswordBox)this.FindName("PasswordBox");
+         _passwordBox.PasswordChanged += _passwordBox_PasswordChanged;
+      }
+
+      private void _passwordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+      {
+         _privateTextBoxViewModel.Password = _passwordBox.Password;
+      }
+
+      private void _viewButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         _privateTextBoxViewModel.TooglePasswordVisibility();
+      }
+
+      private void _viewButton_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         _privateTextBoxViewModel.TooglePasswordVisibility();
+      }
+   }
+}
