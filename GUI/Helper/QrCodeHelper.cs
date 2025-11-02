0561f6fc38721765defc44f05145d2da4866629a
Yassin Lokhat <yassinlokhat@gmail.com>
2025-11-02 21:21:45 +0300

    Adding QrCodeHelper

---
+++ GUI/Helper/QrCodeHelper.cs
@@ -0,0 +1,35 @@
+﻿using QRCodeEncoderLibrary;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Upsilon.Apps.Passkey.GUI.Helper
+{
+   public static class QrCodeHelper
+   {
+      public static Bitmap? GetQrCode(string content)
+      {
+         if (string.IsNullOrEmpty(content))
+         {
+            return null;
+         }
+
+         QREncoder qrGenerator = new()
+         {
+            ErrorCorrection = ErrorCorrection.H
+         };
+         bool[,] matrix = qrGenerator.Encode(content);
+
+         QRSaveBitmapImage qrCodeImage = new(matrix)
+         {
+            ModuleSize = 100,
+            QuietZone = 50
+         };
+
+         MemoryStream ms = new();
+         qrCodeImage.SaveQRCodeToImageFile(ms, ImageFormat.Bmp);
+
+         return (Bitmap)Image.FromStream(ms);
+      }
+   }
+}
