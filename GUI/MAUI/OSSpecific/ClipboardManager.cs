dc193cf54190f5a163b03905dc662a1f0c584b26
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-05-18 09:45:01 +0300

    Boucle auto MDP par rapport au enregistrement de l'util

---
+++ GUI/MAUI/OSSpecific/ClipboardManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Upsilon.Apps.Passkey.Interfaces.Utils;
+
+namespace Upsilon.Apps.Passkey.GUI.MAUI.OSSpecific
+{
+    public class OSSpecificClipboardManager : IClipboardManager
+    {
+        public int RemoveAllOccurence(string[] removeList)
+        {
+            // Sur Windows, vous pourriez mettre votre code complexe.
+            // Pour l'instant, on laisse vide pour que ça compile sur Android/iOS/Windows.
+            return 0;
+        }
+    }
+}
