11a47a9398ad35601884b64031121b9341c3ab61
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-06-24 22:06:31 +0300

    modification to improve database openness

--- GUI/MAUI/ViewModels/MainViewModel.cs
+++ GUI/MAUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Text;
 using Upsilon.Apps.Passkey.Core.Utils;
 using Upsilon.Apps.Passkey.GUI.MAUI.OSSpecific;
@@ -9,7 +9,7 @@ using Upsilon.Apps.Passkey.Interfaces.Utils;
 
 namespace Upsilon.Apps.Passkey.GUI.MAUI.ViewModels
 {
-    internal class MainViewModel : INotifyPropertyChanged
+    internal partial class MainViewModel : ObservableObject
     {
         public static readonly ICryptographyCenter CryptographyCenter = new CryptographyCenter();
         public static readonly ISerializationCenter SerializationCenter = new JsonSerializationCenter();
@@ -18,16 +18,17 @@ namespace Upsilon.Apps.Passkey.GUI.MAUI.ViewModels
 
         public static IDatabase? Database = null;
 
-        public bool IsDatabaseLoaded => Database != null;
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(DatabaseLabel))] 
+        private string _databaseFile = string.Empty;
 
-        private string _databaseLabel = "Aucune base chargée";
-        public string DatabaseLabel
-        {
-            get => _databaseLabel;
-            set { _databaseLabel = value; OnPropertyChanged(nameof(DatabaseLabel)); }
-        }
+        [ObservableProperty]
+        private string _credentialsLabel = "Username :";
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        public string DatabaseLabel => File.Exists(DatabaseFile)
+            ? $"Database : {Path.GetFileName(DatabaseFile)}"
+            : "No database loaded.";
+
+
     }
 }
