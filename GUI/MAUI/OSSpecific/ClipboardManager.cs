99019dae04813c805d60b0b77e935519f87633ed
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-06-24 21:40:29 +0300

    French->enclish and add file .pku

--- GUI/MAUI/OSSpecific/ClipboardManager.cs
+++ GUI/MAUI/OSSpecific/ClipboardManager.cs
@@ -9,8 +9,8 @@ namespace Upsilon.Apps.Passkey.GUI.MAUI.OSSpecific
     {
         public int RemoveAllOccurence(string[] removeList)
         {
-            // Sur Windows, vous pourriez mettre votre code complexe.
-            // Pour l'instant, on laisse vide pour que ça compile sur Android/iOS/Windows.
+            // On Windows, you could put your complex code here.
+            // For now, we leave it empty so it compiles on Android/iOS/Windows
             return 0;
         }
     }
