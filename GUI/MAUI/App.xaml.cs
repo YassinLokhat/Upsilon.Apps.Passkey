ab47401a11a4a5d1f44935313a3ad502003444f2
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-06-24 22:07:32 +0300

    modification for the large screen

--- GUI/MAUI/App.xaml.cs
+++ GUI/MAUI/App.xaml.cs
@@ -13,19 +13,46 @@ namespace MAUI
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
+            window.Title = "Upsilon.Apps.Passkey.GUI.MAUI";
 
-            // On applique la limite UNIQUEMENT si l'application tourne sur Windows ou Mac
-            if (DeviceInfo.Current.Platform == DevicePlatform.WinUI ||
-                DeviceInfo.Current.Platform == DevicePlatform.MacCatalyst)
+            window.Created += (s, e) =>
             {
-                // Limite minimale (Empêche de faire trop petit)
-                window.MinimumWidth = 500;
-                window.MinimumHeight = 600;
+#if WINDOWS
+                // Code Windows fonctionnel
+                var nativeWindow = window.Handler?.PlatformView as Microsoft.UI.Xaml.Window;
+                if (nativeWindow != null)
+                {
+                    var handle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                    var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                    var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                    
+                    if (appWindow.Presenter is Microsoft.UI.Windowing.OverlappedPresenter presenter)
+                    {
+                        presenter.Maximize(); 
+                    }
+                }
+#elif MACCATALYST
+                // Astuce Mac Catalyst : On récupère la scène de la fenêtre
+                var uiWindow = window.Handler?.PlatformView as UIKit.UIWindow;
+                var windowScene = uiWindow?.WindowScene;
+                if (windowScene != null && windowScene.SizeRestrictions != null)
+                {
+                    // On prend les dimensions maximales de l'écran principal de l'utilisateur
+                    var screenSize = UIKit.UIScreen.MainScreen.Bounds.Size;
+                    
+                    // On force temporairement la taille minimale à la taille de l'écran pour l'étirer au max
+                    windowScene.SizeRestrictions.MinimumSize = screenSize;
+                    windowScene.SizeRestrictions.MaximumSize = screenSize;
 
-                // Optionnel : Tu peux aussi définir une taille de démarrage par défaut
-                window.Width = 600;
-                window.Height = 700;
-            }
+                    // On redonne ensuite la liberté à l'utilisateur de redimensionner s'il le veut
+                    Dispatcher.Dispatch(() =>
+                    {
+                        windowScene.SizeRestrictions.MinimumSize = new CoreGraphics.CGSize(500, 600);
+                        windowScene.SizeRestrictions.MaximumSize = new CoreGraphics.CGSize(double.PositiveInfinity, double.PositiveInfinity);
+                    });
+                }
+#endif
+            };
 
             return window;
         }
