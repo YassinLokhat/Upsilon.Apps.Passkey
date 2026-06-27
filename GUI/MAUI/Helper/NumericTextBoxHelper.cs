e95ffc2be0b80bb0f3377a3248bd61aacd737670
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-06-27 13:10:20 +0300

    Add NumericTextBoxHelper

---
+++ GUI/MAUI/Helper/NumericTextBoxHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Maui.Controls;
+
+namespace Upsilon.Apps.Passkey.GUI.MAUI.Helper
+{
+    public static class NumericTextBoxHelper
+    {
+        private static readonly Regex _regex = new("[^0-9]+");
+
+        private static bool _isTextAllowed(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return true;
+            return !_regex.IsMatch(text) && int.TryParse(text, out _);
+        }
+
+        public static void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is not Entry entry) return;
+
+            
+            if (!_isTextAllowed(e.NewTextValue))
+            {
+                
+                entry.Text = e.OldTextValue ?? "";
+            }
+        }
+    }
+}
