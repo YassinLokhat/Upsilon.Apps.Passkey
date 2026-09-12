3686f99044a527de180b2b9c626a06c6b8d8bad8
Yassin Lokhat <yassinlokhat@gmail.com>
2026-09-12 10:52:52 +0300

    Adding blank MAUI project

---
+++ GUI/MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Upsilon.Apps.Passkey.GUI.MAUI
+{
+   [Register("AppDelegate")]
+   public class AppDelegate : MauiUIApplicationDelegate
+   {
+      protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+   }
+}
