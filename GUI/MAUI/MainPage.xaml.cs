d2abcf903900fc34f8fd2daeae4dd632bddb52d4
Yassin Lokhat <yassinlokhat@gmail.com>
2026-09-16 09:22:26 +0300

    Adding MainPageViewModel

--- GUI/MAUI/MainPage.xaml.cs
+++ GUI/MAUI/MainPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿namespace Upsilon.Apps.Passkey.GUI.MAUI
+﻿using Upsilon.Apps.Passkey.GUI.MAUI.ViewModels;
+
+namespace Upsilon.Apps.Passkey.GUI.MAUI
 {
    internal sealed partial class MainPage : ContentPage
    {
-      int _count;
-
       public MainPage()
       {
          InitializeComponent();
-      }
 
-      private void _onCounterClicked(object? sender, EventArgs e)
-      {
-         _count++;
-
-         if (_count == 1)
-            CounterBtn.Text = $"Clicked {_count} time";
-         else
-            CounterBtn.Text = $"Clicked {_count} times";
-
-         SemanticScreenReader.Announce(CounterBtn.Text);
+         BindingContext = new MainPageViewModel();
       }
    }
 }
---
+++ GUI/MAUI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Upsilon.Apps.Passkey.GUI.MAUI.ViewModels
+{
+   internal sealed class MainPageViewModel
+   {
+      public static string AppTitle => Helper.AppInfo.Title;
+
+      /// <summary>
+      /// Window title, optionally including the login idle countdown suffix.
+      /// </summary>
+      public string WindowTitle
+      {
+         get;
+         set;
+      } = AppTitle;
+   }
+}
