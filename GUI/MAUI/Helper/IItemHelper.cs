ba8432cef898ccc7fbad26ec6650063453a16b12
Yassin Lokhat <yassinlokhat@gmail.com>
2026-09-21 16:00:11 +0300

    Remove unused MeetsFilterConditions methods

--- GUI/MAUI/Helper/IItemHelper.cs
+++ GUI/MAUI/Helper/IItemHelper.cs
@@ -10,50 +10,5 @@ namespace Upsilon.Apps.Passkey.GUI.MAUI.Helper
         {
             _ = user.ItemId;
         }
-
-        public static bool MeetsFilterConditions(this IService service, string serviceFilter, string identifierFilter, string globalTextFilter, bool changedItemsOnly)
-        {
-            serviceFilter = serviceFilter.ToLower().Trim();
-            identifierFilter = identifierFilter.ToLower().Trim();
-            globalTextFilter = globalTextFilter.ToLower().Trim();
-
-            string serviceId = service.ItemId.Replace(service.User.ItemId, string.Empty).ToLower().Trim();
-            string serviceName = service.ServiceName.ToLower().Trim();
-            string url = service.Url.ToLower().Trim();
-            string notes = service.Notes.ToLower().Trim();
-
-            return (!string.IsNullOrWhiteSpace(globalTextFilter)
-               ? serviceId == globalTextFilter
-                  || serviceName.Contains(globalTextFilter)
-                  || url.Contains(globalTextFilter)
-                  || notes.Contains(globalTextFilter)
-                  || service.Accounts.Any(x => x.MeetsFilterConditions(string.Empty, globalTextFilter, changedItemsOnly))
-               : (string.IsNullOrWhiteSpace(serviceFilter)
-                     || (!string.IsNullOrWhiteSpace(serviceFilter) && serviceName.Contains(serviceFilter)))
-                  && (string.IsNullOrWhiteSpace(identifierFilter)
-                     || service.Accounts.Any(x => x.MeetsFilterConditions(identifierFilter, globalTextFilter, changedItemsOnly))))
-               && (!changedItemsOnly || service.HasChanged());
-        }
-
-        public static bool MeetsFilterConditions(this IAccount account, string identifierFilter, string globalTextFilter, bool changedItemsOnly)
-        {
-            identifierFilter = identifierFilter.ToLower().Trim();
-            globalTextFilter = globalTextFilter.ToLower().Trim();
-
-            string accountId = account.ItemId.Replace(account.Service.ItemId, string.Empty).ToLower().Trim();
-            string label = account.Label.ToLower().Trim();
-            string notes = account.Notes.ToLower().Trim();
-            string identifiers = string.Join("\n", account.Identifiers.Select(x => x.ToLower().Trim()));
-
-            return (!string.IsNullOrWhiteSpace(globalTextFilter)
-               ? accountId == globalTextFilter
-                  || identifiers.Contains(globalTextFilter)
-                  || label.Contains(globalTextFilter)
-                  || notes.Contains(globalTextFilter) 
-               : string.IsNullOrWhiteSpace(identifierFilter)
-                  || identifiers.Contains(identifierFilter)
-                  || label.Contains(identifierFilter))
-               && (!changedItemsOnly || account.HasChanged());
-        }
     }
 }
