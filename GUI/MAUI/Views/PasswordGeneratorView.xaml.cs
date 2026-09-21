20e793b76b43ad684080d41fa4a5d75503992df3
Nirintsoa_Arsene <ralambozafy3570326@gmail.com>
2026-09-21 15:36:34 +0300

    PasswordGenerator_P1

--- GUI/MAUI/Views/PasswordGeneratorView.xaml.cs
+++ GUI/MAUI/Views/PasswordGeneratorView.xaml.cs
@@ -1,15 +1,15 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Devices;
 using System;
 using System.Threading.Tasks;
-using Upsilon.Apps.Passkey.GUI.MAUI.ViewModels;
 using Upsilon.Apps.Passkey.GUI.MAUI.Helper;
+using Upsilon.Apps.Passkey.GUI.MAUI.ViewModels;
 
 namespace Upsilon.Apps.Passkey.GUI.WPF.Views
 {
     public partial class PasswordGenerator : ContentPage
     {
         private readonly PasswordGeneratorViewModel _viewModel;
-       
         private TaskCompletionSource<string?>? _tcs;
 
         public string? GeneratedPassword { get; private set; } = null;
@@ -19,21 +19,22 @@ namespace Upsilon.Apps.Passkey.GUI.WPF.Views
             InitializeComponent();
             BindingContext = _viewModel = new PasswordGeneratorViewModel();
 
-            // Windows/Desktop-specific resizing
-#if WINDOWS
+            // Ajustement de la taille de fenêtre réservé au Desktop
+#if WINDOWS || MACCATALYST
             this.Loaded += (s, e) =>
             {
-                var window = this.Window;
-                window.Width = 550;
-                window.Height = 450;
+                if (this.Window != null)
+                {
+                    this.Window.Width = 550;
+                    this.Window.Height = 500;
 
-                var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
-                window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
-                window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+                    var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+                    this.Window.X = (displayInfo.Width / displayInfo.Density - this.Window.Width) / 2;
+                    this.Window.Y = (displayInfo.Height / displayInfo.Density - this.Window.Height) / 2;
+                }
             };
 #endif
         }
-
 
         public static async Task<string?> ShowGeneratePasswordDialogAsync(INavigation navigation)
         {
@@ -48,22 +49,45 @@ namespace Upsilon.Apps.Passkey.GUI.WPF.Views
             NumericTextBoxHelper.TextChanged(sender, e);
         }
 
-        
-
         private void _regenerateMenuItem_Click(object sender, EventArgs e)
         {
             _viewModel.GeneratePassword();
         }
 
-        private void _copyMenuItem_Click(object sender, EventArgs e)
+        private async void _copyMenuItem_Click(object sender, EventArgs e)
         {
-            Clipboard.Default.SetTextAsync(_viewModel.GeneratedPassword);
+            if (!string.IsNullOrEmpty(_viewModel.GeneratedPassword))
+            {
+                await Clipboard.Default.SetTextAsync(_viewModel.GeneratedPassword);
+            }
         }
 
+        // Valide la sélection et dépile la vue modale
+        private async void OnSelectPasswordClicked(object sender, EventArgs e)
+        {
+            GeneratedPassword = _viewModel.GeneratedPassword;
+            _tcs?.TrySetResult(GeneratedPassword);
+            await Navigation.PopModalAsync();
+        }
+
+        // Annule et ferme la vue modale
+        private async void OnCancelClicked(object sender, EventArgs e)
+        {
+            _tcs?.TrySetResult(null);
+            await Navigation.PopModalAsync();
+        }
+
+        // Gestion de la touche "Retour" sur Android
         protected override bool OnBackButtonPressed()
         {
             _tcs?.TrySetResult(null);
             return base.OnBackButtonPressed();
+        }
+
+        // Amélioration de l'ergonomie tactile pour le label "Leaked?"
+        private void OnToggleLeakedTapped(object sender, EventArgs e)
+        {
+            LeakedCheckBox.IsChecked = !LeakedCheckBox.IsChecked;
         }
     }
 }
